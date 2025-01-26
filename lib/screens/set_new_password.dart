79117aab69f49cc69d9d444f25bbd1bf55ca6860
souadha986 <sn.hamsas@esi-sba.dz>
2025-01-26 14:06:03 +0100

    changes

--- lib/screens/set_new_password.dart
+++ lib/screens/set_new_password.dart
@@ -1,11 +1,11 @@
-import 'package:app/widgets/custom_back_button.dart';
-import 'package:app/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
+import 'package:app/widgets/custom_back_button.dart';
 import 'package:app/widgets/custom_image_widget.dart';
+import 'package:app/widgets/custom_text_form_field.dart';
 import 'package:app/widgets/custom_text_widget.dart';
 
-class SignIn extends StatelessWidget {
-  const SignIn({super.key});
+class SetNewPassword extends StatelessWidget {
+  const SetNewPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +20,8 @@ class SignIn extends StatelessWidget {
 
               Center(
                 child: CustomImageWidget(
-                  imagePath: 'assets/login/zadi.png',
-                  width: 364,
+                  imagePath: 'assets/login/password.png',
+                  width: 200,
                   height: 170,
                 ),
               ),
@@ -30,20 +30,37 @@ class SignIn extends StatelessWidget {
 
               Center(
                 child: CustomTextWidget(
-                  text: 'Good to see you again!',
+                  text: 'Set new password please',
                   width: double.infinity,
                   height: 50,
                   fontFamily: 'Lobster',
-                  fontSize: 38.0,
+                  fontSize: 35.0,
                   fontWeight: FontWeight.w400,
                   textAlign: TextAlign.center,
                   color: Colors.white,
-                  decoration: TextDecoration.none, // Underline this part
-                  decorationColor: Colors.white,
+                  decoration: TextDecoration.none, 
+                 decorationColor:Colors.white ,
                 ),
               ),
               
-              const SizedBox(height: 60),
+              const SizedBox(height: 10),
+
+              Center(
+                child: CustomTextWidget(
+                  text: 'password must be at least 8 character',
+                  width: 350,
+                  height: 30,
+                  fontFamily: 'Lobster',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w400,
+                  textAlign: TextAlign.center,
+                  color: Colors.white,
+                  decoration: TextDecoration.none, 
+                 decorationColor:Colors.white ,
+                ),
+              ),
+
+              const SizedBox(height: 50),
               
               CustomTextFormField(
                 hinttext: 'E-mail',
@@ -52,7 +69,7 @@ class SignIn extends StatelessWidget {
                 imageWidth: 30,
                 spacing: 16,
               ),
-              
+             
               const SizedBox(height: 25),
               
               CustomTextFormField(
@@ -62,34 +79,14 @@ class SignIn extends StatelessWidget {
                 imageWidth: 30,
                 spacing: 16,
               ),
-             
-              const SizedBox(height: 10),
-             
-             Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () { Navigator.pushNamed(context, '/resetPassword');},
-                  child: CustomTextWidget(
-                    text: 'Forget password?',
-                    width: 200,
-                    height: 30,
-                    fontFamily: 'Lobster',
-                    fontSize: 19,
-                    fontWeight: FontWeight.w400,
-                    textAlign: TextAlign.right,
-                    color: Colors.white,
-                    decoration: TextDecoration.underline, // Underline this part
-                    decorationColor: Colors.white,
-                  ),
-                ),
-              ),
-             
-              const SizedBox(height: 80),
+
+
+              const SizedBox(height: 100),
              
               Center(
                 child: TextButton(
                 onPressed: () {
-               // Action lorsque le bouton est pressé
+               Navigator.pushNamed(context, '/passwordUpdated');
                },
                 style: TextButton.styleFrom(
                backgroundColor: Colors.white,
@@ -97,7 +94,7 @@ class SignIn extends StatelessWidget {
                borderRadius: BorderRadius.circular(30) ),
                padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 10) ),
                child: CustomTextWidget(
-                text: "Sign In",
+                text: "Update",
                 color: const Color(0xFF013220),
                 width: double.infinity,
                 height: 35,
@@ -105,8 +102,8 @@ class SignIn extends StatelessWidget {
                 fontSize: 24,
                fontWeight: FontWeight.w400,
                textAlign: TextAlign.center,
-               decoration: TextDecoration.underline, // Underline this part
-               decorationColor: Colors.white,
+               decoration: TextDecoration.none, 
+                 decorationColor:Colors.white ,
                ),
              ),
             ),
