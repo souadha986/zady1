b381a99e66a1a64191bd77e30f5bc4df97cabce5
soumichaL <soumicha757@gmail.com>
2025-01-26 15:46:34 +0100

    Update welcome_page.dart

--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -25,7 +25,7 @@ class WelcomePage extends StatelessWidget {
               Align(
                 alignment: Alignment.centerLeft,
                 child: CustomTextWidget(
-                  text: 'Are you ?',
+                  text: 'Are youuuuuuuuu ?',
                   width: 200,
                   height: 70,
                   fontFamily: 'Lobster',
