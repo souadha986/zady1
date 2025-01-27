68d5153d7a36cc325a0810ac9c48f50323d0ead4
soumichaL <soumicha757@gmail.com>
2025-01-27 19:31:45 +0100

    Update set_new_password.dart

--- lib/screens/set_new_password.dart
+++ lib/screens/set_new_password.dart
@@ -17,6 +17,7 @@ class SetNewPassword extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const CustomBackButton(),
+
               Center(
                 child: CustomImageWidget(
                   imagePath: 'assets/login/password.png',
@@ -24,7 +25,9 @@ class SetNewPassword extends StatelessWidget {
                   height: 170,
                 ),
               ),
+
               const SizedBox(height: 50),
+
               Center(
                 child: CustomTextWidget(
                   text: 'Set new password please',
@@ -35,11 +38,13 @@ class SetNewPassword extends StatelessWidget {
                   fontWeight: FontWeight.w400,
                   textAlign: TextAlign.center,
                   color: Colors.white,
-                  decoration: TextDecoration.none,
-                  decorationColor: Colors.white,
+                  decoration: TextDecoration.none, 
+                 decorationColor:Colors.white ,
                 ),
               ),
+              
               const SizedBox(height: 10),
+
               Center(
                 child: CustomTextWidget(
                   text: 'password must be at least 8 character',
@@ -50,11 +55,13 @@ class SetNewPassword extends StatelessWidget {
                   fontWeight: FontWeight.w400,
                   textAlign: TextAlign.center,
                   color: Colors.white,
-                  decoration: TextDecoration.none,
-                  decorationColor: Colors.white,
+                  decoration: TextDecoration.none, 
+                 decorationColor:Colors.white ,
                 ),
               ),
+
               const SizedBox(height: 50),
+              
               CustomTextFormField(
                 hinttext: 'E-mail',
                 imagePath: 'assets/login/email.png',
@@ -62,7 +69,9 @@ class SetNewPassword extends StatelessWidget {
                 imageWidth: 30,
                 spacing: 16,
               ),
+             
               const SizedBox(height: 25),
+              
               CustomTextFormField(
                 hinttext: 'Password',
                 imagePath: 'assets/login/lock.png',
@@ -70,32 +79,34 @@ class SetNewPassword extends StatelessWidget {
                 imageWidth: 30,
                 spacing: 16,
               ),
+
+
               const SizedBox(height: 100),
+             
               Center(
                 child: TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/passwordUpdated');
-                  },
-                  style: TextButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30)),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 70, vertical: 10)),
-                  child: CustomTextWidget(
-                    text: "Update",
-                    color: const Color(0xFF013220),
-                    width: double.infinity,
-                    height: 35,
-                    fontFamily: 'Lobster',
-                    fontSize: 24,
-                    fontWeight: FontWeight.w400,
-                    textAlign: TextAlign.center,
-                    decoration: TextDecoration.none,
-                    decorationColor: Colors.white,
-                  ),
-                ),
-              ),
+                onPressed: () {
+               Navigator.pushNamed(context, '/passwordUpdated');
+               },
+                style: TextButton.styleFrom(
+               backgroundColor: Colors.white,
+               shape: RoundedRectangleBorder(
+               borderRadius: BorderRadius.circular(30) ),
+               padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 10) ),
+               child: CustomTextWidget(
+                text: "Update",
+                color: const Color(0xFF013220),
+                width: double.infinity,
+                height: 35,
+                fontFamily: 'Lobster',
+                fontSize: 24,
+               fontWeight: FontWeight.w400,
+               textAlign: TextAlign.center,
+               decoration: TextDecoration.none, 
+                 decorationColor:Colors.white ,
+               ),
+             ),
+            ),
             ],
           ),
         ),
