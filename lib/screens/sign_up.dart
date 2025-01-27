e6127f944877b8cd5cd172f5ef1fb60f34f69dd2
souadha986 <149321404+souadha986@users.noreply.github.com>
2025-01-27 20:25:51 +0100

    changes

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -4,33 +4,30 @@ import 'package:zady/widgets/custom_image_widget.dart';
 import 'package:zady/widgets/custom_text_form_field.dart';
 import 'package:zady/widgets/custom_text_widget.dart';
 
-
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: SingleChildScrollView(
-     child:Padding(padding: const EdgeInsets.all(20),
-     child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const CustomBackButton(),
-           
-        const Center(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CustomBackButton(),
+              const Center(
                 child: CustomImageWidget(
                   imagePath: 'assets/login/zadi.png',
                   width: 364,
                   height: 170,
                 ),
               ),
-         
-        const SizedBox(height: 10),
-
+              const SizedBox(height: 10),
               const Center(
                 child: CustomTextWidget(
-                  text: ' Become a client',
+                  text: ' Become a client anfel',
                   width: double.infinity,
                   height: 50,
                   fontFamily: 'Lobster',
@@ -42,12 +39,12 @@ class SignUp extends StatelessWidget {
                   decorationColor: Colors.white,
                 ),
               ),
-               const SizedBox(height: 5),
-                const Align(
-                  alignment:Alignment.centerRight,
+              const SizedBox(height: 5),
+              const Align(
+                alignment: Alignment.centerRight,
                 child: CustomTextWidget(
                   text: 'create your account and get started whith us ',
-                  width:349,
+                  width: 349,
                   height: 30,
                   fontFamily: 'Lobster',
                   fontSize: 17.0,
@@ -58,10 +55,7 @@ class SignUp extends StatelessWidget {
                   decorationColor: Colors.white,
                 ),
               ),
-
-
-               const SizedBox(height: 60),
-              
+              const SizedBox(height: 60),
               const CustomTextFormField(
                 hinttext: 'E-mail',
                 imagePath: 'assets/login/email.png',
@@ -69,9 +63,7 @@ class SignUp extends StatelessWidget {
                 imageWidth: 30,
                 spacing: 16,
               ),
-
               const SizedBox(height: 25),
-              
               const CustomTextFormField(
                 hinttext: 'Username',
                 imagePath: 'assets/login/utilisateur.png',
@@ -79,9 +71,7 @@ class SignUp extends StatelessWidget {
                 imageWidth: 30,
                 spacing: 16,
               ),
-              
               const SizedBox(height: 25),
-              
               const CustomTextFormField(
                 hinttext: 'Password',
                 imagePath: 'assets/login/lock.png',
@@ -89,9 +79,7 @@ class SignUp extends StatelessWidget {
                 imageWidth: 30,
                 spacing: 16,
               ),
-
               const SizedBox(height: 25),
-              
               const CustomTextFormField(
                 hinttext: 'Confirm Password',
                 imagePath: 'assets/login/lock.png',
@@ -99,51 +87,36 @@ class SignUp extends StatelessWidget {
                 imageWidth: 30,
                 spacing: 16,
               ),
-
-
-             const SizedBox(height: 80),
-             Center(
+              const SizedBox(height: 80),
+              Center(
                 child: TextButton(
-                onPressed: () {
-               // Action lorsque le bouton est pressé
-               },
-                style: TextButton.styleFrom(
-               backgroundColor: Colors.white,
-               shape: RoundedRectangleBorder(
-               borderRadius: BorderRadius.circular(30) ),
-               padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 10) ),
-               child: CustomTextWidget(
-                text: "Verify",
-                color: const Color(0xFF013220),
-                width: double.infinity,
-                height: 35,
-                fontFamily: 'Lobster',
-                fontSize: 24,
-               fontWeight: FontWeight.w400,
-               textAlign: TextAlign.center,
-               decoration: TextDecoration.underline, // Underline this part
-               decorationColor: Colors.white,
-               ),
-             ),
-            ),
-              
-           
-
-
-      ],
-     ), 
-    
-
-
-     ) ,
-
-
-     ),
-
-
-
-
-
+                  onPressed: () {
+                    // Action lorsque le bouton est pressé
+                  },
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 70, vertical: 10)),
+                  child: CustomTextWidget(
+                    text: "Verify",
+                    color: const Color(0xFF013220),
+                    width: double.infinity,
+                    height: 35,
+                    fontFamily: 'Lobster',
+                    fontSize: 24,
+                    fontWeight: FontWeight.w400,
+                    textAlign: TextAlign.center,
+                    decoration: TextDecoration.underline, // Underline this part
+                    decorationColor: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
