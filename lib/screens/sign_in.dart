f2b5bec3011417cab2172b08a3ea89aa9f1cdeb0
soumichaL <soumicha757@gmail.com>
2025-01-27 19:26:43 +0100

    Update sign_in.dart

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -17,6 +17,7 @@ class SignIn extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const CustomBackButton(),
+
               Center(
                 child: CustomImageWidget(
                   imagePath: 'assets/login/zadi.png',
@@ -24,7 +25,9 @@ class SignIn extends StatelessWidget {
                   height: 170,
                 ),
               ),
+
               const SizedBox(height: 50),
+
               Center(
                 child: CustomTextWidget(
                   text: 'Good to see you again!',
@@ -35,11 +38,13 @@ class SignIn extends StatelessWidget {
                   fontWeight: FontWeight.w400,
                   textAlign: TextAlign.center,
                   color: Colors.white,
-                  decoration: TextDecoration.none, // Underline this part
+                  decoration: TextDecoration.none, 
                   decorationColor: Colors.white,
                 ),
               ),
+              
               const SizedBox(height: 60),
+              
               CustomTextFormField(
                 hinttext: 'E-mail',
                 imagePath: 'assets/login/email.png',
@@ -47,7 +52,9 @@ class SignIn extends StatelessWidget {
                 imageWidth: 30,
                 spacing: 16,
               ),
+              
               const SizedBox(height: 25),
+              
               CustomTextFormField(
                 hinttext: 'Password',
                 imagePath: 'assets/login/lock.png',
@@ -55,53 +62,54 @@ class SignIn extends StatelessWidget {
                 imageWidth: 30,
                 spacing: 16,
               ),
+             
               const SizedBox(height: 10),
-              Align(
+             
+             Align(
                 alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/resetPassword');
-                  },
+                child: GestureDetector(
+                  onTap: () { Navigator.pushNamed(context, '/resetPassword');},
                   child: CustomTextWidget(
                     text: 'Forget password?',
-                    width: 200,
+                    width: 145,
                     height: 30,
                     fontFamily: 'Lobster',
                     fontSize: 19,
                     fontWeight: FontWeight.w400,
                     textAlign: TextAlign.right,
                     color: Colors.white,
-                    decoration: TextDecoration.underline, // Underline this part
+                    decoration: TextDecoration.underline, 
                     decorationColor: Colors.white,
                   ),
                 ),
               ),
+             
               const SizedBox(height: 80),
+             
               Center(
                 child: TextButton(
-                  onPressed: () {
-                    // Action lorsque le bouton est pressé
-                  },
-                  style: TextButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30)),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 70, vertical: 10)),
-                  child: CustomTextWidget(
-                    text: "Sign In",
-                    color: const Color(0xFF013220),
-                    width: double.infinity,
-                    height: 35,
-                    fontFamily: 'Lobster',
-                    fontSize: 24,
-                    fontWeight: FontWeight.w400,
-                    textAlign: TextAlign.center,
-                    decoration: TextDecoration.underline, // Underline this part
-                    decorationColor: Colors.white,
-                  ),
-                ),
-              ),
+                onPressed: () {
+               // homepage
+               },
+                style: TextButton.styleFrom(
+               backgroundColor: Colors.white,
+               shape: RoundedRectangleBorder(
+               borderRadius: BorderRadius.circular(30) ),
+               padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 10) ),
+               child: CustomTextWidget(
+                text: "Sign In",
+                color: const Color(0xFF013220),
+                width: double.infinity,
+                height: 35,
+                fontFamily: 'Lobster',
+                fontSize: 24,
+               fontWeight: FontWeight.w400,
+               textAlign: TextAlign.center,
+               decoration: TextDecoration.none, 
+               decorationColor: Colors.white,
+               ),
+             ),
+            ),
             ],
           ),
         ),
